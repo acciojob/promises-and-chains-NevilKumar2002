9b1c9078611294125fab00fa11146c4424ffe86a
NevilKumar2002 <127580266+NevilKumar2002@users.noreply.github.com>
2023-09-01 07:58:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const name=document.getElementById("name");
+const age=document.getElementById("age");
+const button=document.getElementById("btn");
+
+async function callme()
+	{
+		if((age.value)>18){
+		let x=await new Promise((resolve,reject )=>{
+		
+				setTimeout(()=>{
+					let text=`Welcome, ${name.value}. You can vote.`
+					resolve(text);
+					
+				},4000)
+			
+			});
+			x.then(()=>{
+				alert(text);
+			})
+		}
+		else
+		{
+			alert(`Oh sorry ${name.value}. You aren't old enough`)
+		}
+		
+	}
+button.addEventListener('click',callme)
